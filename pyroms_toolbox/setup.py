c55ab1c6ec9804fd41d46d73d5b9956507076265
dyk <dyk@dhigroup.com>
2025-02-21 14:22:38 -0700

    add toolbox packages

--- pyroms_toolbox/setup.py
+++ pyroms_toolbox/setup.py
@@ -3,5 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="pyroms_toolbox",
     version="0.1.0",
-    packages=['pyroms_toolbox'],
+    packages=[
+        'pyroms_toolbox',
+        'pyroms_toolbox.BGrid_GFDL',
+        'pyroms_toolbox.BGrid_POP',
+        'pyroms_toolbox.BGrid_SODA',
+        'pyroms_toolbox.CGrid_GLORYS',
+        'pyroms_toolbox.Grid_GLORYS',
+        'pyroms_toolbox.Grid_HYCOM',
+        'pyroms_toolbox.seawater',
+    ],
 )
